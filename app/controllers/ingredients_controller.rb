10de7d6d52d634db6d4c80b94ba6dbb7464cf5d3
DanRWilloughby <DanRWilloughby@gmail.com>
2015-02-05 21:22:52 -0600

    Week 4 HW

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,10 @@
+class IngredientsController < ApplicationController
+
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def show
+    @ingredient = Ingredient.find_by(id: params["id"])
+  end
+end
